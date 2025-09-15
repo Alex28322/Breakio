5665a9a679c2e9dfd57fc4d6aca0ff0690a9f42c
Alex28322 <sander2832@gmail.com>
2025-09-15 14:33:34 +0300

    polished

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
-
-    let position = 0;
-const line = document.querySelector('.users__scroll');
-const scrollwidth = line.scrollWidth
-
-document.querySelector('.button__right-gray').addEventListener('click', function(){
-    position = position + scrollwidth / 4;
-    if (position > (3 * scrollwidth) / 4){
-        position = 0
-    }
-    line.style.left = - position + 'px' 
-});
-document.querySelector('.button__left-gray').addEventListener('click', function(){
-    position = position - scrollwidth / 4;
-    if (position < 0){
-        position = (3 * scrollwidth) / 4
-    }
-    line.style.left = - position + 'px'
-});
+const swiper = new Swiper('.swiper', {
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    spaceBetween: 1,
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
