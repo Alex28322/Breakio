97817591500da0470331384e68b3fdc5d0cd20ee
Alex28322 <sander2832@gmail.com>
2025-09-15 14:34:16 +0300

    polished

--- js/script.js
+++ js/script.js
@@ -23,3 +23,43 @@ const swiper = new Swiper('.swiper', {
       el: '.swiper-scrollbar',
     },
   });
+const items = document.querySelectorAll('.question__item-box');
+
+items.forEach((item) => {
+  const arrow = item.querySelector('.question__item-arrow');
+  const answer = item.querySelector('.question__item-text-main');
+  if (!arrow || !answer) return;
+
+  arrow.addEventListener('click', () => {
+    const isOpen = arrow.classList.contains('active');
+
+    if (isOpen) {
+      arrow.classList.remove('active');
+      answer.classList.remove('active');
+      answer.style.maxHeight = '0px';
+    } else {
+      arrow.classList.add('active');
+      answer.classList.add('active');
+      answer.style.maxHeight = answer.scrollHeight + 'px';
+    }
+  });
+});
+const triggerBtns = document.querySelectorAll('.button-form');
+const overlay = document.querySelector('.overlay');
+const form = document.querySelector('.signin__form')
+
+triggerBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    overlay.classList.add('show');
+    form.classList.add('active');
+    document.body.style.overflow = 'hidden';
+  });
+});
+
+overlay.addEventListener('click', (e) => {
+  if (e.target === overlay) {
+    overlay.classList.remove('show');
+     form.classList.remove('active');
+     document.body.style.overflow = '';
+  }
+});
